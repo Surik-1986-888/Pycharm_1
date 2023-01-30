b2ee36c23533fe84532a63735ec461adb63acb73
Surik <sur_mkrtchyan@mail.ru>
2023-01-30 21:08:32 +0400

    Pycharm_1

---
+++ main.py
@@ -0,0 +1,11 @@
+text = 'File Write Python Operations - Read and  to files with Python'
+x = text.split()
+lengs_world = len(x[0])
+world = str()
+print(lengs_world)
+for i in x:
+	if len(i) >= lengs_world:
+		lengs_world = len(i)
+		world = i
+print(lengs_world)
+print('longest word of txt file is', world)
